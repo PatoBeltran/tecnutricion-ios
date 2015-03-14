b4ba3fd5bb7c5837914ffe20f8511c91f3bca86d
PatoBeltran <pbelsal@gmail.com>
2015-03-13 18:38:23 -0600

    Initial Commit

---
+++ NutreTec/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NutreTec
+//
+//  Created by Patricio Beltrán on 3/13/15.
+//  Copyright (c) 2015 Tecnologico de Monterrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NutreTec/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NutreTec
+//
+//  Created by Patricio Beltrán on 3/13/15.
+//  Copyright (c) 2015 Tecnologico de Monterrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
