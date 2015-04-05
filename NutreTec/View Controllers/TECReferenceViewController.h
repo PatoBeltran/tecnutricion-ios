951c1d383297bb8ac295f8a2813a49024080feee
PatoBeltran <pbelsal@gmail.com>
2015-04-05 03:03:53 -0500

    Add table for reference view

---
+++ NutreTec/View Controllers/TECReferenceViewController.h
@@ -0,0 +1,13 @@
+//
+//  TECReferenceViewController.h
+//  NutreTec
+//
+//  Created by Patricio Beltrán on 4/5/15.
+//  Copyright (c) 2015 Tecnologico de Monterrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TECReferenceViewController : UIViewController
+
+@end
